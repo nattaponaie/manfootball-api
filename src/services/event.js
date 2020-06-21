ed1d9104e606a3ab8324482e5b555fdc4be80cc5
Nattapon Aiemlaor <nattapon.aie@gmail.com>
2020-06-22 00:37:52 +0700

    implement event service

---
+++ src/services/event.js
@@ -0,0 +1,19 @@
+const create = (eventModel, eventMessageText) => {
+  const splitedMsg = eventMessageText.split(' ') ;
+  if (splitedMsg.length !== 3) {
+    throw new Error('คำสั่งผิดจ้า /สร้าง (สถานที่) (เวลา)');
+  }
+
+  if (eventModel.getIsCreated()) {
+    throw new Error('มีคนสร้างแล้วน้า บวกได้เลย (พิมพ์ /+1)');
+  }
+  
+  eventModel.setLocation(splitedMsg[1]);
+  eventModel.setTime(splitedMsg[2]);
+  eventModel.setIsCreated(true);
+  return eventModel.getEventDesc();
+};
+
+export default {
+  create,
+};
