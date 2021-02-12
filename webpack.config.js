64d1520ebf188d34536fd3f58f7c180beef0cfee
nattapontoey <nattapon.aie@gmail.com>
2021-02-12 17:26:26 +0700

    setup typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,60 @@ const webpack = require('webpack');
 const path = require('path');
 const NodemonPlugin = require('nodemon-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
+const TSLintPlugin = require('tslint-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 const plugins = [
   new Dotenv(),
   new webpack.NamedModulesPlugin(),
   new NodemonPlugin(),
   new webpack.HotModuleReplacementPlugin(),
+  new UglifyJSPlugin(),
+  new TSLintPlugin({
+    files: ['./src/**/*.ts'],
+  }),
 ];
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  devtool: false,
+  devtool: 'source-map',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/,
+        },
+      },
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true,
+    },
+  },
   externals: [
     nodeExternals(),
   ],
   name: 'server',
   plugins: plugins,
   target: 'node',
-  entry: [path.resolve(__dirname, 'src/index.js')],
+  entry: [path.resolve(__dirname, 'src/index.ts')],
   output: {
-    publicPath: './',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    chunkFilename: '[name].js',
+    filename: '[name].js',
   },
   resolve: {
-    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx', '.ts'],
     modules: ['node_modules'],
+    alias: {
+      'utils': path.resolve('./src/utils'),
+      'middleware': path.resolve('./src/middleware'),
+      'templates': path.resolve('./src/templates'),
+      'models': path.resolve('./src/models'),
+      'services': path.resolve('./src/services'),
+      'database': path.resolve('./src/database'),
+    },
   },
   module: {
     rules: [
@@ -43,6 +70,11 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?/,
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
